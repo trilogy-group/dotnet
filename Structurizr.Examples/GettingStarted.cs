9276ad98161a4b38f31592eaab685be69c01e770
Simon Brown <simon.brown@codingthearchitecture.com>
2017-07-03 14:34:10 +0100

    Made the "getting started" example consistent with the Java version.

--- Structurizr.Examples/GettingStarted.cs
+++ Structurizr.Examples/GettingStarted.cs
@@ -2,12 +2,23 @@ using Structurizr.Client;
 
 namespace Structurizr.Examples
 {
+
+    /// <summary>
+    /// A "getting started" example that illustrates how to
+    /// create a software architecture diagram using code.
+    /// 
+    /// The live workspace is available to view at https://structurizr.com/share/25441
+    /// </summary>
     class GettingStarted
     {
 
+        private const long WorkspaceId = 25441;
+        private const string ApiKey = "key";
+        private const string ApiSecret = "secret";
+
         static void Main(string[] args)
         {
-            Workspace workspace = new Workspace("My model", "This is a model of my software system.");
+            Workspace workspace = new Workspace("Getting Started", "This is a model of my software system.");
             Model model = workspace.Model;
 
             Person user = model.AddPerson("User", "A user of my software system.");
@@ -15,16 +26,16 @@ namespace Structurizr.Examples
             user.Uses(softwareSystem, "Uses");
 
             ViewSet viewSet = workspace.Views;
-            SystemContextView contextView = viewSet.CreateSystemContextView(softwareSystem, "context", "A simple example of a System Context diagram.");
+            SystemContextView contextView = viewSet.CreateSystemContextView(softwareSystem, "context", "An example of a System Context diagram.");
             contextView.AddAllSoftwareSystems();
             contextView.AddAllPeople();
 
             Styles styles = viewSet.Configuration.Styles;
             styles.Add(new ElementStyle(Tags.SoftwareSystem) { Background = "#1168bd", Color = "#ffffff" });
-            styles.Add(new ElementStyle(Tags.Person) { Background = "#08427b", Color = "#ffffff" });
+            styles.Add(new ElementStyle(Tags.Person) { Background = "#08427b", Color = "#ffffff", Shape = Shape.Person });
 
-            StructurizrClient structurizrClient = new StructurizrClient("key", "secret");
-            structurizrClient.PutWorkspace(1234, workspace);
+            StructurizrClient structurizrClient = new StructurizrClient(ApiKey, ApiSecret);
+            structurizrClient.PutWorkspace(WorkspaceId, workspace);
         }
 
     }
