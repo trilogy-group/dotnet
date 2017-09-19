8618bd2f8b8cf1e1422f6805cb6ab65930fa7960
Simon Brown <simon.brown@codingthearchitecture.com>
2017-09-19 19:58:23 +0100

    Added a version of the StructurizrClient that can be used when the Structurizr on-premises installation resides behind Azure AD.

--- Structurizr.Core/Api/HttpHeaders.cs
+++ Structurizr.Core/Api/HttpHeaders.cs
@@ -5,6 +5,7 @@
 
         internal const string UserAgent = "User-Agent";
         internal const string Authorization = "Authorization";
+        internal const string XAuthorization = "X-Authorization";
         internal const string ContentType = "Content-Type";
         internal const string ContentMd5 = "Content-MD5";
         internal const string Nonce = "Nonce";
